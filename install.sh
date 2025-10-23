40a259fb93942bad4bfde471772a6a4a036d137c
Wesley <wesleymcginn1@gmail.com>
2025-10-23 00:00:02 -0400

    Eliminate need to download repository
    Running the install script will download the necessary programming automatically.

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 echo "Installing necessary libraries...."
 sudo apt install python3-picamera2 -y
 
-echo "Done.  Setting script to automatically start on boot...."
-sudo mv ./pivestream.py /usr/local/bin/pivestream.py
+echo "Done.  Downloading livestream script...."
+sudo wget -O /usr/local/bin/pivestream.py https://wesleymcginn.github.io/Pivestream/pivestream.py
+
+echo "Done.  Setting script to start automatically on boot...."
 if crontab -l 2>/dev/null | grep -Fq "@reboot python3 /usr/local/bin/pivestream.py"; then
-    echo "This is already set to run on startup."
+    echo "This is already set to run on boot."
 else
     (crontab -l 2>/dev/null; echo "@reboot python3 /usr/local/bin/pivestream.py") | crontab -
 fi
-python3 /usr/local/bin/pivestream.py
-echo "Done!  Your pivestream is up and running!"
+
+echo "Done!  Reboot to start your livestream system."
