be6a657fac4011b8b13c3ba6f0da532e6a8d4d1d
Wesley <wesleymcginn1@gmail.com>
2025-10-23 22:42:57 -0400

    Make user output prettier

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Installing necessary libraries...."
-sudo apt install python3-picamera2 -y
+sudo apt-get install python3-picamera2 -y > /dev/null
 
-echo "Done.  Downloading livestream script...."
-sudo wget -O /usr/local/bin/pivestream.py https://wesleymcginn.github.io/Pivestream/pivestream.py
+echo "Downloading livestream script...."
+sudo wget -O /usr/local/bin/pivestream.py -q https://wesleymcginn.github.io/Pivestream/pivestream.py
 
-echo "Done.  Setting script to start automatically on boot...."
-if crontab -l 2>/dev/null | grep -Fq "@reboot python3 /usr/local/bin/pivestream.py"; then
+echo "Setting script to start automatically on boot...."
+if crontab -l 2>/dev/null | grep -qF "@reboot python3 /usr/local/bin/pivestream.py"; then
     echo "This is already set to run on boot."
 else
     (crontab -l 2>/dev/null; echo "@reboot python3 /usr/local/bin/pivestream.py") | crontab -
 fi
 
-echo "Done!  Reboot to start your livestream system."
+echo "Done!  Reboot your pi and then access the livestream from http://$(hostname -I | tr -d ' '):7000"
