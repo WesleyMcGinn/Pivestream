77b4554fdda6a80c27d7243e5a05f0cc70000ff1
Wesley <wesleymcginn1@gmail.com>
2026-06-18 12:22:58 -0400

    Make look nicer

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-echo "Installing necessary libraries...."
+echo -e "\e[30;103;1mInstalling dependencies....\e[0m"
 sudo apt-get install python3-picamera2 -y > /dev/null
 
-echo "Downloading livestream script...."
+echo "\e[30;103;1mDownloading livestream script....\e[0m"
 sudo wget -O /usr/local/bin/pivestream.py -q https://wesleymcginn.github.io/Pivestream/pivestream.py
 
-echo "Setting script to start automatically on boot...."
+echo "\e[30;103;1mSetting script to start automatically on boot....\e[0m"
 if crontab -l 2>/dev/null | grep -qF "@reboot python3 /usr/local/bin/pivestream.py"; then
-    echo "This is already set to run on boot."
+    echo "Pivestream is already set to run on boot."
 else
     (crontab -l 2>/dev/null; echo "@reboot python3 /usr/local/bin/pivestream.py") | crontab -
 fi
 
-echo -e "\e[32m"
-echo "Done!  Reboot your pi and then access the livestream from http://$(ip -4 -br addr show | awk '/^wl/ && NF>=3 { split($3, a, "/"); print a[1] }'):7000"
-echo -e "\e[0m"
+echo -e "\e[97;42;1mDone!  Reboot your pi and then access the livestream from http://$(ip -4 -br addr show | awk '/^wl/ && NF>=3 { split($3, a, "/"); print a[1] }'):7000\e[0m"
