e474f97280aa4b423e86cb186328ff122d5b5e17
Wesley <wesleymcginn1@gmail.com>
2026-06-18 12:56:58 -0400

    Create an install script for USB Webcam
    The beginning of a new era!

---
+++ install.usb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\e[30;103;1mInstalling dependencies....\e[0m"
+sudo apt-get install python3-opencv -y > /dev/null
+
+echo -e "\e[30;103;1mDownloading livestream script....\e[0m"
+sudo wget -O /usr/local/bin/pivestream.usb.py -q https://wesleymcginn.github.io/Pivestream/pivestream.usb.py
+
+echo -e "\e[30;103;1mSetting script to start automatically on boot....\e[0m"
+if crontab -l 2>/dev/null | grep -qF "@reboot python3 /usr/local/bin/pivestream.usb.py"; then
+    echo "Pivestream USB is already set to run on boot."
+else
+    (crontab -l 2>/dev/null; echo "@reboot python3 /usr/local/bin/pivestream.usb.py") | crontab -
+fi
+
+echo -e "\e[97;42;1mDone!  Reboot your pi and then access the livestream from http://$(ip -4 -br addr show | awk '/^wl/ && NF>=3 { split($3, a, "/"); print a[1] }'):7000\e[0m"
+
+read -p "Wanna reboot now? [Y/n] " response
+if [[ "$response" =~ ^[yY] ]]; then
+    echo -e "\e[93mRebooting in 3 seconds.\e[0m"
+    echo -e "Press \e[31;1mCtrl + c\e[0m to cancel. "
+    sleep 3.5
+    sudo reboot
+else
+    echo "OK.  Reboot whenever you're ready."
+fi
